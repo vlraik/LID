528650a3f78fb2e45210eaabc53995e8c5094207
rowhanm <rohanmahadev7@gmail.com>
2016-03-13 19:08:17 +0530

    Test changes

--- helloworld.py
+++ helloworld.py
@@ -6,10 +6,3 @@ print "merging files is magical"
 print "and tough to understand too"
 print "though i still need to learn a bit about it"
 
-<<<<<<< HEAD
-print "maybe i finally learnt how to do that"
-print "still trying to understand"
-=======
-print "so if i try to merge like this then"
-print "still trying to understand"
->>>>>>> origin/master
