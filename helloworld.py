7486ac8a584c4472bdbdbb33f32340333eef4280
vlraik <vikylakhani@gmail.com>
2016-03-13 15:15:20 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,8 @@
 print "hello world"
 print "hihihihihihi"
-=======
 print "hello"
 print "this is awesome"
-<<<<<<< HEAD
 print "merging files is magical"
-=======
+print "and tough to understand too"
 print "though i still need to learn a bit about it"
->>>>>>> origin/master
+
